dcd65cc2bd2d6ae502e8380a307716c8b69e047d
Aleksandr Zharkevich <alexandr.zharkevich@gmail.com>
2020-02-01 12:27:37 +0300

    FirstApp.py modified

--- FirstApp.py
+++ FirstApp.py
@@ -16,6 +16,7 @@ def BubbleSort(A):  # сортировка пузырьком
 def QuickSort(A, fst, lst):  # быстрая сортировка
     if fst >= lst:
         return
+
     # i, j = fst, lst
     pivot = A[fst]
     # pivot = A[random.randint(fst, lst)]
@@ -30,6 +31,7 @@ def QuickSort(A, fst, lst):  # быстрая сортировка
             A[i], A[first_bigger] = A[first_bigger], A[i]
             first_bigger += 1
         i += 1
+
     last_smaller = first_bigger - 1
     A[fst], A[last_smaller] = A[last_smaller], A[fst]
     QuickSort(A, fst, last_smaller - 1)
@@ -48,13 +50,35 @@ for N in range(1000, 5001, 1000):
     A = []
     for i in range(N):
         A.append(int(round(random.random() * (max - min) + min)))
-    print(A)
+
+    # print(A)
+
     B = A.copy()
-    print(B)
+    # print(B)
+
+    # BubbleSort(A)
+    print("---")
+    # print(A)
+
+    # QuickSort(B, 0, len(B)-1)
+    print("---")
+    # print(B)
+
+    t1 = datetime.datetime.now()
     BubbleSort(A)
-    print("---")
-    print(A)
-    QuickSort(B, 0, len(B)-1)
-    print("---")
-    print(B)
-    print("\n\n")
+    t2 = datetime.datetime.now()
+    y1.append((t2 - t1).total_seconds())
+    print("Пузырьковая сортировка   " + str(N) + "   заняла   " + str((t2 - t1).total_seconds()) + "c")
+
+    t3 = datetime.datetime.now()
+    QuickSort(B, 0, len(B) - 1)
+    t4 = datetime.datetime.now()
+    y2.append((t4 - t3).total_seconds())
+    print("Быстрая   " + str(N) + "   заняла   " + str((t4 - t3).total_seconds()) + "c")
+
+    table.add_row([str(N), str((t2 - t1).total_seconds()), str((t4 - t3).total_seconds())])
+print(table)
+
+plt.plot(x, y1, "C0")
+plt.plot(x, y2, "C1")
+plt.show()
